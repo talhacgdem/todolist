62d1bcd32f616a2d9f0cd0ba0fa3d99659f13f84
MTC <talhacgdem@hotmail.com>
2022-10-28 19:06:51 +0300

    util package edited

---
+++ src/main/java/com/talhacgdem/todolist/util/DateUtil.java
@@ -0,0 +1,23 @@
+package com.talhacgdem.todolist.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+
+public class DateUtil {
+
+    public static String getLocatedDateString(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy, EEEE");
+        return date.format(formatter);
+    }
+
+    public static LocalDate getFirstDayOfWeekFromThisWeek(){
+        LocalDate today = LocalDate.now();
+        return today.with(ChronoField.DAY_OF_WEEK, 1);
+    }
+
+    public static LocalDate getLastDayOfWeekFromThisWeek(){
+        return getFirstDayOfWeekFromThisWeek().plusDays(6);
+    }
+
+}
